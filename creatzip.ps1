b0d9f847b5cc5e6f2a26a32dc69c2fb8c50bc58f
TZockt <info@tzockt.de>
2024-01-09 19:33:11 +0100

    add createzip script

---
+++ creatzip.ps1
@@ -0,0 +1,18 @@
+# Aktuellen Arbeitsordner als Quellpfad verwenden
+$sourcePath = Get-Location
+$zipFileName = "manifest.zip"
+$zipFilePath = Join-Path -Path $sourcePath -ChildPath $zipFileName
+
+# Überprüfen, ob die Zip-Datei bereits existiert, und löschen
+if (Test-Path $zipFilePath) {
+    Remove-Item $zipFilePath -Force
+    Write-Host "Vorhandenes Archiv gelöscht: $zipFileName"
+}
+
+# Liste der zu inkludierenden Dateien
+$includedFiles = @("manifest.json", "logo.png", "README.md")
+
+# Zippen der ausgewählten Dateien
+Compress-Archive -Path $includedFiles -DestinationPath $zipFilePath
+
+Write-Host "Erfolgreich in $zipFileName gepackt."
